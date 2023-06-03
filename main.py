29c28a6cc8a77f86c95cca558c4c08fb1398ca2f
moeinTavakoli <mtawakoli2283@gmail.com>
2023-06-03 11:09:28 +0330

    [ Init ] Initialize main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import pytesseract
+
+def picture_to_text(picture_path):
+    image = Image.open(picture_path)
+    image = image.convert('L')
+    image = image.point(lambda x:0 if x < 128 else 255 , "1")
+
+    text = pytesseract.image_to_string(image)
+
+    print(text)
+    
+    
+picture_to_text("/path/to/picture.png")
