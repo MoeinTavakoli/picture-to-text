c4ce7cc44fd0f42bd19533168cdb92d1cee2d229
moeinTavakoli <mtawakoli2283@gmail.com>
2023-06-03 11:57:27 +0330

    [ Feature ] update function --> add try , except

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from PIL import Image
 import pytesseract
+from os import path
 
 def picture_to_text(picture_path):
-    image = Image.open(picture_path)
-    image = image.convert('L')
-    image = image.point(lambda x:0 if x < 128 else 255 , "1")
-
-    text = pytesseract.image_to_string(image)
-
-    return text
+    try:
+        if not (path.isfile(picture_path)):
+            raise ValueError('file not found !!!') 
+        image = Image.open(picture_path)
+        image = image.convert('L')
+        image = image.point(lambda x:0 if x < 128 else 255 , "1")
+        text = pytesseract.image_to_string(image)
+        return text
+    except ValueError as exp:
+        print("Error", exp) 
     
 picture_path = input('please insert picture path : ')
 text = picture_to_text(picture_path)
