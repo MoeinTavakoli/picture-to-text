7a9b9fc37cebde6abf04237942222590d050048d
moeinTavakoli <mtawakoli2283@gmail.com>
2023-06-03 12:21:22 +0330

    [ Feature ] update documentation --> add comment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import library and functions 
 from PIL import Image
 import pytesseract
 from os import path
@@ -7,10 +8,15 @@ import sys
 
 def picture_to_text():
     try:
+        # Check argument and must be pass the file 
+        # For example
+        # python main.py ~/source-code/python/picture-to-text/test.png
         if len(sys.argv) != 2:
             raise ValueError('There isn\'t and picture pass as argument !') 
-        
+
         picture_path = sys.argv[1]
+        # Check the file is exist or not 
+        # If file didn't exist , generate Error 
         if not (path.isfile(picture_path)):
             raise ValueError('file not found !!!') 
         image = Image.open(picture_path)
